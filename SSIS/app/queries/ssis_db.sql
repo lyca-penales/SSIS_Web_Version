8e8a9326a409d9504d9d9fd784c59074c64c4f4d
Lyca B. Penales <79522017+lyca-penales@users.noreply.github.com>
2022-01-22 15:04:08 +0800

    Add Photo_Link variable in student table

---
+++ SSIS/app/queries/ssis_db.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE ssis_db;
+USE ssis_db;
+CREATE TABLE college (
+	College_Code VARCHAR(50) NOT NULL PRIMARY KEY,
+    College_Name VARCHAR(250) NOT NULL
+    );
+
+INSERT INTO college(College_Code, College_Name)
+	VALUES
+		('CCS', 'College of Computer Studies'),
+        ('CSM', 'College of Science and Mathematics'),
+        ('COET', 'College of Engineering and Technology');
+        
+CREATE TABLE courses (
+	Course_Code VARCHAR(50) NOT NULL PRIMARY KEY,
+	Course_Name VARCHAR(250) NOT NULL,
+	College_Code VARCHAR(50),
+    FOREIGN KEY (College_Code) REFERENCES college (College_Code)
+    );
+    
+INSERT INTO courses(Course_Code, Course_Name)
+	VALUES
+		('BSCS', 'Bachelor of Science in Computer Science'),
+        ('BSM', 'Bachelor of Science in Mathematics'),
+        ('BSEE', 'Bachelor of Science in Eletrical Engineering');
+        
+CREATE TABLE student (
+	Student_Id VARCHAR(10) NOT NULL PRIMARY KEY,
+    Photo_Link VARCHAR(250) NOT NULL,
+    Firstname VARCHAR(250) NOT NULL,
+    Lastname VARCHAR(250) NOT NULL,
+    Gender VARCHAR(200) NOT NULL,
+    Year_Level VARCHAR(200) NOT NULL,
+    Course_Code VARCHAR(50),
+    FOREIGN KEY (Course_Code) REFERENCES courses (Course_Code)
+    );
+    
+INSERT INTO student(Student_Id, Photo_Link, Firstname, Lastname, Gender, Year_Level)
+	VALUES
+		('2019-0001', 'https://res.cloudinary.com/dccav3ldg/image/upload/v1642721953/SSIS/user-icon-placeholder_lzfbmn.png', 'Chris', 'Thompson', 'Male', '3rd Year'),
+        ('2018-0001', 'https://res.cloudinary.com/dccav3ldg/image/upload/v1642721953/SSIS/user-icon-placeholder_lzfbmn.png', 'Jane', 'Perez', 'Female', '4th Year'),
+        ('2020-0001', 'https://res.cloudinary.com/dccav3ldg/image/upload/v1642721953/SSIS/user-icon-placeholder_lzfbmn.png', 'Josh', 'Lopez', 'Male', '2nd Year');
